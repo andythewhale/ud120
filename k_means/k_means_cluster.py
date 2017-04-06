1c75c616920c10aace5c9d8243c2227df398c7f6
Andy Miller <andymiller1313@gmail.com>
2017-04-06 04:59:45 -0500

    Finished project on KMeans clustering

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,8 +48,9 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2,feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -58,12 +59,22 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2,f3 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+
+from sklearn.cluster import KMeans
+import numpy as np
+X = finance_features
+kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
+kmeans.labels_
+
+pred = kmeans.predict(finance_features)
+
+kmeans.cluster_centers_
 
 
 
@@ -74,3 +85,20 @@ try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
+
+
+esoList = []
+for key, value in data_dict.iteritems():
+    
+    if value['exercised_stock_options'] != 'NaN':
+        esoList.append(value['exercised_stock_options'])
+print "minimum exercised stock options: ", min(esoList)
+print "maximum exercised stock options: ", max(esoList)
+
+salaryList = []
+for key, value in data_dict.iteritems():
+    
+    if value['salary'] != 'NaN':
+        salaryList.append(value['salary'])
+print "minimum salary: ", min(salaryList)
+print "maximum salary: ", max(salaryList)
